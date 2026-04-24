71349991c0fdbbfb2a5716fc62415dd4c3ccd80e
K Pradeep Kumar Reddy <kpradeepkumarreddy@gmail.com>
2026-04-24 11:28:01 +0530

    Create init_database.sql for database setup
    This script creates databases for bronze, silver, gold layers and a logs database, while ensuring existing databases are dropped. It also sets up a load_log table for tracking loading processes.

---
+++ scripts/init_database.sql
@@ -0,0 +1,33 @@
+/*
+================================================================================
+    Create Databases
+=================================================================================
+Script Purpose:
+    This script creates databases for each layer: bronze, silver, gold, and also creates a database for storing logs after checking if they already exists. 
+    
+WARNING:
+    Running this script will drop all the databases if they exist. 
+    All data in the database will be permanently deleted. Proceed with caution 
+    and ensure you have proper backups before running this script.
+*/
+
+CREATE DATABASE IF NOT EXISTS datawarehouse_bronze;
+CREATE DATABASE IF NOT EXISTS datawarehouse_silver;
+CREATE DATABASE IF NOT EXISTS datawarehouse_gold;
+CREATE DATABASE IF NOT EXISTS datawarehouse_logs;
+
+
+
+USE datawarehouse_logs;
+
+DROP TABLE IF EXISTS load_log;
+CREATE TABLE load_log (
+    log_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    batch_id VARCHAR(100),              -- groups one full run
+    table_name VARCHAR(100),           -- table being loaded
+    load_start_time DATETIME,
+    load_end_time DATETIME,
+    duration_seconds INT,
+    rows_loaded INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
