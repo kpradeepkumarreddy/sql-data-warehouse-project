1a0532067892ef1c34ec90e2fffb7878af300517
K Pradeep Kumar Reddy <kpradeepkumarreddy@gmail.com>
2026-04-25 14:49:03 +0530

    Update crm_sales_details table to use DATE type
    Changed data types for order dates in crm_sales_details table.

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -37,15 +37,15 @@ CREATE TABLE crm_prd_info (
 
 DROP TABLE IF EXISTS crm_sales_details;
 CREATE TABLE crm_sales_details (
-    sls_ord_num  VARCHAR(50),
-    sls_prd_key  VARCHAR(50),
-    sls_cust_id  INT,
-    sls_order_dt INT,
-    sls_ship_dt  INT,
-    sls_due_dt   INT,
-    sls_sales    INT,
-    sls_quantity INT,
-    sls_price    INT,
+    sls_ord_num		VARCHAR(50),
+    sls_prd_key		VARCHAR(50),
+    sls_cust_id		INT,
+	sls_order_dt	DATE,
+    sls_ship_dt		DATE,
+    sls_due_dt		DATE,
+    sls_sales		INT,
+    sls_quantity	INT,
+    sls_price		INT,
     dwh_create_date DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
